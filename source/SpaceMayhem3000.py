7f9f4b254e1f7e043362107eaf42a7a6cf888e55
Marek Veverka <veverka@spstrutnov.cz>
2020-02-04 18:40:12 +0100

    úpravy čitelnosti (wip)

--- source/SpaceMayhem3000.py
+++ source/SpaceMayhem3000.py
@@ -3,32 +3,21 @@ import random
 import sys
 
 
-resolution_x = 1650 
-resolution_y = 850 
-pohled = pygame.display.set_mode((resolution_x,resolution_y))
+dis_width = 1650 
+dis_height = 850 
+pohled = pygame.display.set_mode((dis_width,dis_height))
 pygame.init()
 move = True 
 #POSOTIONS
-playerx = 825
-playery = 725
 enemyx = random.randint(0,1600)
 enemyy = random.randint(0,700)
 shot_velocity = 15
 rate_of_fire = 3
-vv = 5
-hv = 7
-def mov():
-    global playerx
-    global playery
-    key = pygame.key.get_pressed()
-    if key[pygame.K_UP]:
-        playery -= vv
-    if key[pygame.K_DOWN]:
-        playery += vv
-    if key[pygame.K_LEFT]:
-        playerx -= hv
-    if key[pygame.K_RIGHT]:
-        playerx += hv
+ship_vv = 5
+ship_hv = 7
+ship_x = 825
+ship_y = 725
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, v):
         pygame.sprite.Sprite.__init__(self)
@@ -42,18 +31,44 @@ class Enemy(pygame.sprite.Sprite):
         
     def move(self):
         if move == True:
-            global enemyv
+            global enemy_v
             self.y -= self.v
             self.rect.y = self.y
 
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, x, y):
         self.image = pygame.image.load('assets/sship.png').convert_alpha()
+        pygame.sprite.Sprite.__init__(self)
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.x = x
+        self.rect.y = y
+        self.y = y
+        self.x = x
+        
+    def mov():
+        if move == True:
+            global ship_x
+            global ship_y
+            key = pygame.key.get_pressed()
+            if key[pygame.K_UP]:
+                ship_y -= ship_vv
+            if key[pygame.K_DOWN]:
+                ship_y += ship_vv
+            if key[pygame.K_LEFT]:
+                ship_x -= ship_hv
+            if key[pygame.K_RIGHT]:
+                ship_x += ship_hv
+            if ship_x > dis_width - 50:
+                ship_x = dis_width - 50
+            if ship_x < 0:
+                ship_x = 0
+            if ship_y > dis_height - 50:
+                ship_y = dis_height - 50
+            if ship_y < 0:
+                ship_y = 0
+                
         
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -64,6 +79,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.y = y
         self.x = x
         self.y = y
+        
     def move(self):
         if move == True:
             self.y -= shot_velocity
@@ -75,7 +91,37 @@ def exitbutton():
         if event.type == pygame.QUIT :
             pygame.quit()
             quit()
+            
+def collision_detection():
+    collision_with_enemy = pygame.sprite.spritecollide(player, enemy_group, True, pygame.sprite.collide_rect_ratio(0.7))
+    
+    if collision_with_enemy:
+        move = False
+        pohled.blit(game_over,(0,0))
 
+def menu_buttons():
+    global level
+    pohled.blit(bkg, (0,0)) #pozadí
+    pohled.blit(play, (500,65)) #tlacitko_play
+    pohled.blit(credit, (500, 330)) #tlacitko_credits
+    pohled.blit(end, (500, 595)) #tlacitko_quit
+    
+    cursor = pygame.mouse.get_pos()
+    
+    if cursor[0] > 500 and cursor[0] < 1150 and cursor[1] > 65 and cursor[1] < 265:
+        pohled.blit(plays,(500,65))
+        if pygame.mouse.get_pressed()[0]:
+            level = 1
+    elif cursor[0] > 500 and cursor [0] < 1150 and cursor[1]  > 330 and cursor[1] < 550:
+        pohled.blit(creditss,(500,330))
+        if pygame.mouse.get_pressed()[0]:
+            #bud se dodělávat později, potřebuji zjistit jak vyčíst ze souboru.
+            pass
+    elif cursor[0] > 500 and cursor[0] < 1150 and cursor[1] > 615 and cursor[1] < 815:
+        pohled.blit(quits,(500,595))
+        if pygame.mouse.get_pressed()[0]:
+            exit()
+    pygame.display.update()
 
 
 
@@ -108,7 +154,7 @@ shots = []
 rate_count = 0
 enemies = []
 
-enemyv = 5
+enemy_v = 5
 
 move = True
 
@@ -116,68 +162,41 @@ move = True
 while level == 0:
     
     exitbutton()
-    pohled.fill(white)
-    pohled.blit(bkg, (0,0)) #pozadí
-    pohled.blit(play, (500,65)) #tlacitko_play
-    pohled.blit(credit, (500, 330)) #tlacitko_credits
-    pohled.blit(end, (500, 595)) #tlacitko_quit
-    cursor = pygame.mouse.get_pos()
-    
-    if cursor[0] > 500 and cursor[0] < 1150 and cursor[1] > 65 and cursor[1] < 265:
-        pohled.blit(plays,(500,65))
-        if pygame.mouse.get_pressed()[0]:
-            level = 1
-    elif cursor[0] > 500 and cursor [0] < 1150 and cursor[1]  > 330 and cursor[1] < 550:
-        pohled.blit(creditss,(500,330))
-        if pygame.mouse.get_pressed()[0]:
-            #bud se dodělávat později, potřebuji zjistit jak vyčíst ze souboru.
-            pass
-    elif cursor[0] > 500 and cursor[0] < 1150 and cursor[1] > 615 and cursor[1] < 815:
-        pohled.blit(quits,(500,595))
-        if pygame.mouse.get_pressed()[0]:
-            exit()
+    menu_buttons()
     pygame.display.update()
+    
 while level == 1:
-    enemycount = 3
     exitbutton()
     pohled.blit(game,(0,0))
-    shotx = playerx - 6.5
-    shoty = playery - 15
-    enemyv = 3
-    enemyonscreen = 0
-    max_enemy = 3
     
+    enemy_x = random.randint(0, dis_width - 50)
+    enemy_y = random.randint(0, dis_height - 50)
+    shotx = ship_x + 18
+    shoty = ship_y
     
-    player = Player([playerx, playery])
+    max_enemies = 10
+    
+    #GENERACE HRACE
+    player = Player(ship_x, ship_y)
     player_group = pygame.sprite.Group()
     player_group.add(player)
-    enemy = Enemy(enemyx, enemyy, enemyv)
-    enemies.append(enemy)
+    
+    enemy = Enemy(enemy_x, enemy_y, 15)
     enemy_group = pygame.sprite.Group()
+    enemy_group.add(enemy)
+    enemy_group.draw(pohled)
     
     shot_group = pygame.sprite.Group()
 
-
     player_group.draw(pohled)
+    
     key = pygame.key.get_pressed()
     
-    #umozneni
-    if move == True:
-        mov()
-
-    for enemy in enemies:
-        enemy.move()
-        if enemyonscreen < max_enemy:
-            enemy_group.add(enemy)
-            enemy_group.draw(pohled)
-            enemyonscreen += 1
-            if enemy.y < 0:
-                enemy.v *= -1
-            if enemy.y > 800:
-                enemy.v *= -1
-
-
+    #POHYB HRACE - FINALNI VERZE
+    Player.mov()
     
+        
+    #STRILENI
     shot = Bullet(shotx,shoty)
     if key[pygame.K_SPACE] and rate_count > rate_of_fire:
         shots.append(shot)
@@ -198,30 +217,7 @@ while level == 1:
             move = False
     else:
         shot_group.draw(pohled)
-
-        
-        
-    collision_with_enemy = pygame.sprite.spritecollide(player, enemy_group, True, pygame.sprite.collide_rect_ratio(0.7))
     
-    if collision_with_enemy:
-        move = False
-        pohled.blit(game_over,(0,0))
+    collision_detection()
  
-        
-    if playerx > 1625:
-        playerx = 1625
-    if playerx < 25:
-        playerx = 25
-    if playery > 825:
-        playery = 825
-    if playery < 25:
-        playery = 25
-        
-
-            
-    
-    
-    
-    
-    
     pygame.display.update()
